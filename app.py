ded81f9111d646ff6270194850ce71dc7451e6fc
ZoneOfChaos <zoneofchaos@protonmail.com>
2022-12-11 18:56:06 +0300

    First commit

---
+++ app.py
@@ -0,0 +1,14 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def hello_world():  # put application's code here
+    return 'Hello World!'
+@app.route('/user/<string:name>/<int:id>')
+def user(name, id):
+    return "User page" + name + "--" + str(id)
+
+if __name__ == '__main__':
+    app.run(debug=True)
