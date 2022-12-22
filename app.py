3429c0b76480ca30c96151fc04a2dfdf9e2d5b8b
ZoneOfChaos <zoneofchaos@protonmail.com>
2022-12-22 17:15:57 +0300

    add bootstrap styles

--- app.py
+++ app.py
@@ -1,14 +1,12 @@
-from flask import Flask #first project
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 
 @app.route('/')
-def hello_world():  # put application's code here
-    return 'Hello World!'
-@app.route('/user/<string:name>/<int:id>')
-def user(name, id):
-    return "User page" + name + "--" + str(id)
+def index():  # put application's code here
+    return render_template("index.html")
+
 
 if __name__ == '__main__':
     app.run(debug=True)
