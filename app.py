31a74d389215cf88f0f5f0697511a3b243460e8b
Zone_Guard <ZoneOfChaos@protonmail.com>
2023-03-19 13:29:22 +0300

    add API

--- app.py
+++ app.py
@@ -4,6 +4,21 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():  # put application's code here
+
+
+    url = "https://weatherapi-com.p.rapidapi.com/current.json"
+
+    querystring = {"q":"Moscow"}
+
+    headers = {
+        "X-RapidAPI-Key": "988a3fca0fmsh09ec1fe9f3d9263p1ec578jsne8d811aac699",
+        "X-RapidAPI-Host": "weatherapi-com.p.rapidapi.com"
+    }
+
+    response = requests.request("GET", url, headers=headers, params=querystring)
+
+    print(response.text)
+
     return render_template("index.html")
 
 
